dadd4af10dc87597f0b64745a08b84836a7de640
junginLee0130 <dlwjddls0130@naver.com>
2024-06-03 20:07:53 +0900

    update: member enum Role 수정

--- marketapi/src/main/java/com/example/marketapi/domain/member/Role.java
+++ marketapi/src/main/java/com/example/marketapi/domain/member/Role.java
@@ -1,15 +1,5 @@
 package com.example.marketapi.domain.member;
 
 public enum Role {
-    User("회원");
-
-    final private String role;
-
-    Role(String role) {
-        this.role = role;
-    }
-
-    String getRole(){
-        return role;
-    }
+    User, GUEST;
 }
