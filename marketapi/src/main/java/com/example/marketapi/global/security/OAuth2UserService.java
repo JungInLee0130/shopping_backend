b40f718ac70924118b6c7fe04cca1a674be4d105
junginLee0130 <dlwjddls0130@naver.com>
2024-08-28 20:14:03 +0900

    feat: securityconfig passwordEncoder, webSecurityCustomizer 추가

--- marketapi/src/main/java/com/example/marketapi/global/security/OAuth2UserService.java
+++ marketapi/src/main/java/com/example/marketapi/global/security/OAuth2UserService.java
@@ -1,4 +1,4 @@
-package com.example.marketapi.login.service;
+package com.example.marketapi.global.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
--- marketapi/src/main/java/com/example/marketapi/global/security/SecurityConfig.java
+++ marketapi/src/main/java/com/example/marketapi/global/security/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.example.marketapi.global.security;
 
-import com.example.marketapi.login.service.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
@@ -23,16 +28,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeHttpRequests(config -> config.anyRequest().permitAll());
-        http.oauth2Login(oauth2Configurer -> oauth2Configurer
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+        http.formLogin();
+        /*http.oauth2Login(oauth2Configurer -> oauth2Configurer
                 .loginPage("/login")
                 .successHandler(successHandler())
                 .userInfoEndpoint()
-                .userService(oAuth2UserService)
-        );
+                .userService(oAuth2UserService));*/
+
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        log.info("-------------web configure----------------");
+
+        // 정적 리소스 시큐리티 적용 x
+        return (web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
