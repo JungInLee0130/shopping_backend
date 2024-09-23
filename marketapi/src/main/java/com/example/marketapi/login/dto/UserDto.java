a4e9788b88c499ba81e40ec75a9a11a2398baeea
junginLee0130 <dlwjddls0130@naver.com>
2024-09-23 23:08:10 +0900

    feat: jwt login 완성중

---
+++ marketapi/src/main/java/com/example/marketapi/login/dto/UserDto.java
@@ -0,0 +1,26 @@
+package com.example.marketapi.login.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+}
---
+++ marketapi/src/main/java/com/example/marketapi/post/controller/PostController.java
@@ -0,0 +1,5 @@
+package com.example.marketapi.post.controller;
+
+
+public class PostController {
+}
